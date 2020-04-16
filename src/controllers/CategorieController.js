37531b085293efd46f9a99ef5220e77a086a5cea
Maicon Costa <maiconcostawd@hotmail.com>
2020-04-16 16:49:27 -0300

    List categories route

---
+++ src/controllers/CategorieController.js
@@ -0,0 +1,12 @@
+const { Categorie } = require('../models');
+
+module.exports = {
+    async index(req, res) {
+        await Categorie.findAll()
+            .then((categories) => {
+                return res.json(categories);
+            }).catch((err) => {
+                return res.status(400).json(err);
+            });
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 
+const CategorieController = require('./controllers/CategorieController');
 const IngredientController = require('./controllers/IngredientController');
 const ItemController = require('./controllers/ItemController');
 const OrderController = require('./controllers/OrderController');
@@ -23,5 +24,7 @@ routes.delete('/order/:id', OrderController.delete);
 routes.get('/order/:id', OrderDetailController.index);
 
 routes.get('/status', StatusController.index);
+
+routes.get('/categories', CategorieController.index);
 
 module.exports = routes;
